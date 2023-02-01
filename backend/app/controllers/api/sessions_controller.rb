306f00742aa7eec053bf75a0d2a8ed0efad02540
Kin Ka Tse <kinkatse@Kins-MacBook-Air.local>
2023-02-01 12:17:34 -0500

    first commit

---
+++ backend/app/controllers/api/sessions_controller.rb
@@ -0,0 +1,26 @@
+class Api::SessionsController < ApplicationController
+  def show
+   @user = current_user
+  
+   render 'api/users/show' if @user
+    # current_user ? render json: {user: current_user} :  render json: {user: nil}
+
+  end
+
+  def create
+
+    @user = User.find_by_credentials(params[:credential], params[:password])
+
+    if @user
+      login!(@user)
+      render json: 'api/users/show'
+    else
+      render json: {errors: ['The provided credentials were invalid.']}, status: :unauthorized
+    end
+  end
+
+  def destroy
+    logout! if current_user
+    render json: ['you have successfuly logged out :)']
+  end
+end
